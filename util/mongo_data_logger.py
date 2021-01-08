b14ed039defb7bed709d8f35871e0d784680f305
MedadRufus <medadrufus@gmail.com>
2021-01-08 06:21:08 +0000

    add in unique identifier of server that saves data

--- util/mongo_data_logger.py
+++ util/mongo_data_logger.py
@@ -1,6 +1,7 @@
 # mongo relatedMongoLogger
 from pymongo import MongoClient
 import urllib.parse
+import uuid
 
 # init mongo connection
 username = urllib.parse.quote_plus('dbUser')
@@ -13,11 +14,18 @@ env_file = None
 
 class MongoLogger:
     def __init__(self):
-        pass
+        self.server_uuid = self.get_server_uuid()
 
     def log_data(self, data_dict: dict):
+
+        data_dict["computer_id"] = self.server_uuid
         data_dict["arg_file"] = env_file
         mycol.insert_one(data_dict)
+
+    def get_server_uuid(self):
+        computer_id = uuid.UUID(int=uuid.getnode())
+        return computer_id
+
 
 
 def init_mongo_logger(envfile):
